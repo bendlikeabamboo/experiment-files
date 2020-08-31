eee2023138cc4c23d402e6ae67399805ea47ca2f
Manuel Balmeo <mbalmeo32@gmail.com>
2020-08-31 16:35:45 +0800

    adding these dash examples

---
+++ dash-1/dash-2.py
@@ -0,0 +1,34 @@
+import dash
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    children=[
+        dcc.Input(id='input', value='Enter Something Here', type='text'),
+        html.Div(id='output')
+    ]
+)
+
+
+@app.callback(
+    Output(component_id='output', component_property='children'),
+    [
+        Input(component_id='input', component_property='value')
+    ]
+)
+
+def update_value(input_data):
+    try:
+        return str(float(input_data)**2)
+    except:
+        return "some error."
+
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
